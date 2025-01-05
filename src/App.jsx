482fc5287aa291924ad4a30b5425ecdabb3987fc
procompots <procompot64@gmail.com>
2025-01-05 16:18:19 +0500

    rasm qoshdm

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <h6>Lorem, ipsum dolor.</h6>
         <h6>Lorem, ipsum dolor.</h6>
         <h4>Lorem ipsum dolor sit.</h4>
-        <img src="https://next-images.123rf.com/index/_next/image/?url=https://assets-cdn.123rf.com/index/static/assets/top-section-bg.jpeg&w=3840&q=75" alt="" />
+        <img className='rasm' src="https://next-images.123rf.com/index/_next/image/?url=https://assets-cdn.123rf.com/index/static/assets/top-section-bg.jpeg&w=3840&q=75" alt="" />
 
       </div>
     </>
