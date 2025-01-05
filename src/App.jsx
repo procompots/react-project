a708e9f3e411ad3308f76a88b2bab679f472353c
procompots <procompot64@gmail.com>
2025-01-05 16:07:28 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+function App() {
+  return (
+    <>
+      <div>
+        <h1>hello</h1>
+        <h2>hello albulaziz bot</h2>
+        <a href="#">mars space</a>
+        <h6>Lorem, ipsum dolor.</h6>
+        <h6>Lorem, ipsum dolor.</h6>
+        <h6>Lorem, ipsum dolor.</h6>
+        <h6>Lorem, ipsum dolor.</h6>
+        <h6>Lorem, ipsum dolor.</h6>
+        <h6>Lorem, ipsum dolor.</h6>
+      </div>
+    </>
+  )
+}
+
+
+export default App
