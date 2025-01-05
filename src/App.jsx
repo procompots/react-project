bc61dbeba2f8759e889944dd7c9de6579af6bd47
procompots <procompot64@gmail.com>
2025-01-05 16:15:52 +0500

    rasm qoshdm

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ function App() {
         <h6>Lorem, ipsum dolor.</h6>
         <h6>Lorem, ipsum dolor.</h6>
         <h4>Lorem ipsum dolor sit.</h4>
+        <img src="https://next-images.123rf.com/index/_next/image/?url=https://assets-cdn.123rf.com/index/static/assets/top-section-bg.jpeg&w=3840&q=75" alt="" />
+
       </div>
     </>
   )
